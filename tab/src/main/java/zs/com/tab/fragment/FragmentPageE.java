eb871420b1c3b43892f632407f8633ead3871092
lost <shu_qingzhang@foxmail.com>
2016-07-15 22:52:52 +0800

    add source

---
+++ tab/src/main/java/zs/com/tab/fragment/FragmentPageE.java
@@ -0,0 +1,21 @@
+package zs.com.tab.fragment;
+
+import android.view.LayoutInflater;
+import android.widget.ListView;
+
+/**
+ * Created by zhangshuqing on 16/7/10.
+ */
+public class FragmentPageE extends BaseRefreshFragment {
+
+
+    @Override
+    public void refresh() {
+
+    }
+
+    @Override
+    public void init(ListView listView, LayoutInflater inflater) {
+
+    }
+}
